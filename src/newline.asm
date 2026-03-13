d6a6216be50c6efab30f0c8f5b7071d4f309aa41
veronalaraalan <verona.lara.alan@gmail.com>
2026-03-13 16:45:57 -0600

    Implement newline function to print line break
    Refactor newline function to print a newline character.

--- src/newline.asm
+++ src/newline.asm
@@ -1,16 +1,31 @@
+; =========================================
+; Descripción:
+;   Imprime un salto de línea en pantalla.
+;
+; Entrada:
+;   Ninguna
+;
+; Salida:
+;   Ninguna
+; =========================================
+
 extern print_char
 global newline
 
 SECTION .text
 
+; -----------------------------------------
+; newline
+; Imprime un salto de línea
+; -----------------------------------------
 newline:
 
-    push ebp
-    mov ebp, esp
+    push ebp                    ; guardar el valor anterior de EBP
+    mov ebp, esp                ; crear marco de pila de la función
 
-    ; TODO:
-    ; imprimir '\n'
+    mov al, 10                  ; ASCII 10 = salto de línea '\n'
+    call print_char             ; reutilizar la función print_char
 
-    mov esp, ebp
-    pop ebp
-    ret
+    mov esp, ebp                ; restaurar el puntero de pila
+    pop ebp                     ; restaurar el EBP anterior
+    ret                         ; regresar al programa llamador
