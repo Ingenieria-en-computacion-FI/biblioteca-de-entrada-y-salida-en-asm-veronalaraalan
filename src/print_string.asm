136470410fd4a7e8737e27db561c43bb6d938f33
veronalaraalan <verona.lara.alan@gmail.com>
2026-03-13 16:44:35 -0600

    Implement print_string function to output a string
    Added functionality to print a null-terminated string by calculating its length and using a system call to write it to stdout.

--- src/print_string.asm
+++ src/print_string.asm
@@ -1,22 +1,52 @@
+; =========================================
+; Descripción:
+;   Imprime una cadena terminada en cero.
+;
+; Entrada:
+;   EAX = dirección de la cadena
+;
+; Salida:
+;   Ninguna
+; =========================================
+
 SECTION .text
 global print_string
 
-; ---------------------------------
+; -----------------------------------------
 ; print_string
+; Imprime una cadena terminada en 0
 ; Entrada:
-;   EAX = direccion de cadena
-;   cadena terminada en 0
-; ---------------------------------
-
+;   EAX = dirección de la cadena
+; -----------------------------------------
 print_string:
 
-    push ebp
-    mov ebp, esp
+    push ebp                    ; guardar el valor anterior de EBP
+    mov ebp, esp                ; crear marco de pila de la función
 
-    ; TODO:
-    ; 1. calcular longitud
-    ; 2. syscall write
+    mov esi, eax                ; ESI recorrerá la cadena desde su inicio
+    xor edx, edx                ; EDX funcionará como contador de longitud = 0
 
-    mov esp, ebp
-    pop ebp
-    ret
+count_loop:
+    ; Revisar si ya llegamos al terminador nulo
+    cmp byte [esi], 0
+    je do_print
+
+    ; Si no es fin de cadena, avanzar al siguiente carácter
+    inc esi
+    inc edx
+    jmp count_loop
+
+do_print:
+    ; Llamada al sistema write
+    ; eax = 4  -> sys_write
+    ; ebx = 1  -> stdout
+    ; ecx = dirección de la cadena
+    ; edx = longitud calculada
+    mov ecx, eax
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+
+    mov esp, ebp                ; restaurar el puntero de pila
+    pop ebp                     ; restaurar el EBP anterior
+    ret                         ; regresar al programa llamador
